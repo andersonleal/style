36666af8328f0b86b829f23db7eb3795d14a5935
Peter T Bosse II <pbosse@gmail.com>
2019-05-13 16:32:30 -0400

    Add `api/index`

---
+++ src/api/index.js
@@ -0,0 +1,13 @@
+export { camelCase } from "../utils/camel-case.js"
+export { kebabCase } from "../utils/kebab-case.js"
+export {
+  canMerge,
+  cloneObj,
+  emptyObj,
+  isArr,
+  isObj,
+  merge,
+  mergeArr,
+  mergeObj
+} from "../utils/merge.js"
+export { toPairs } from "../utils/to-pairs.js"
