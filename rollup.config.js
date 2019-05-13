eff13b89f599a7c86dc1eeeeb6598423f886afb3
Peter T Bosse II <pbosse@gmail.com>
2019-05-13 16:41:15 -0400

    Add Rollup

---
+++ rollup.config.js
@@ -0,0 +1,53 @@
+import { execSync } from "child_process"
+import { terser } from "rollup-plugin-terser"
+import pkg from "./package.json"
+
+const banner = `/*! @copyright Peter T Bosse II | @license Apache-2.0 | @link github.com/ptb/style | @version ${new Date ()
+  .toISOString ()
+  .split ("T")
+  .shift ()
+  .replace (/-/gu, ".")}-${execSync ("git rev-parse HEAD")
+  .toString ()
+  .slice (0, 7)} */`
+
+export default [
+  {
+    "input": "src/api/index.js",
+    "output": {
+      "banner": banner,
+      "file": pkg.module,
+      "format": "esm"
+    },
+    "plugins": [
+      terser ({
+        "mangle": {
+          "toplevel": true
+        },
+        "output": {
+          "comments": /^!/u
+        },
+        "sourcemap": false
+      })
+    ]
+  },
+  {
+    "input": "src/api/index.js",
+    "output": {
+      "banner": banner,
+      "file": pkg.main,
+      "format": "umd",
+      "name": "amory"
+    },
+    "plugins": [
+      terser ({
+        "mangle": {
+          "toplevel": true
+        },
+        "output": {
+          "comments": /^!/u
+        },
+        "sourcemap": false
+      })
+    ]
+  }
+]
