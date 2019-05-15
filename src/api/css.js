1600c4c8ff2c47e9275f5521b309740ad870f2f5
Peter T Bosse II <pbosse@gmail.com>
2019-05-15 16:25:42 -0400

    Update `api/css`

--- src/api/css.js
+++ src/api/css.js
@@ -1,5 +1,13 @@
+import { getClassName } from "../build/get-class-name.js"
+import { update } from "../client/update.js"
 import { parse } from "../parse/parse.js"
+import { cache } from "../store/cache.js"
 
 export function css (params = {}) {
   return parse ({ "input": params })
+    .map (cache)
+    .map (update)
+    .map (getClassName)
+    .filter (Boolean)
+    .join (" ")
 }
--- src/api/css.test.js
+++ src/api/css.test.js
@@ -1,9 +1,69 @@
 import ava from "ava"
+import { store } from "../store/store.js"
 import { css } from "./css.js"
+
+function strMapToObj (strMap) {
+  const obj = Object.create (null)
+
+  for (const [k, v] of strMap) {
+    obj[k] = v
+  }
+
+  return obj
+}
 
 ava ("given undefined arguments", (t) => {
   const actual = css ()
-  const expect = []
+  const expect = ""
 
   t.deepEqual (actual, expect)
+})
+
+ava ("given an object with simple declarations", (t) => {
+  const actual1 = css ({
+    "backgroundColor": "#f00",
+    "display": "block"
+  })
+
+  const expect1 = "jt2a9 drtx9"
+
+  const actual2 = strMapToObj (store.get (""))
+
+  const expect2 = {
+    '[{"background-color":"#f00"}]': {
+      "block": [
+        {
+          "background-color": "#f00"
+        }
+      ],
+      "emit": true,
+      "identifier": "jt2a9",
+      "input": {
+        "backgroundColor": "#f00"
+      },
+      "media": "",
+      "property": "backgroundColor",
+      "selectors": [[".jt2a9"]],
+      "value": "#f00"
+    },
+    '[{"display":"block"}]': {
+      "block": [
+        {
+          "display": "block"
+        }
+      ],
+      "emit": true,
+      "identifier": "drtx9",
+      "input": {
+        "display": "block"
+      },
+      "media": "",
+      "property": "display",
+      "selectors": [[".drtx9"]],
+      "value": "block"
+    }
+  }
+
+  t.is (actual1, expect1)
+  t.deepEqual (actual2, expect2)
 })
