e4f2a97a80ce736a011d9375e4d92ef3d6409389
Peter T Bosse II <pbosse@gmail.com>
2019-05-26 19:12:01 -0400

    Update client/update to use insertRule

--- src/client/update.js
+++ src/client/update.js
@@ -1,23 +1,5 @@
-import { canUseDom, getStyleElement, getStyles, store } from "../api/index.js"
+import { insertRule } from "../api/index.js"
 
-/* istanbul ignore next */
-
-export const update = (function (elements) {
-  return function (params = {}) {
-    store.forEach (function (_, media) {
-      canUseDom &&
-        window.requestAnimationFrame (function () {
-          const styles = getStyles (media)
-
-          const style = elements.has (media)
-            ? elements.get (media)
-            : getStyleElement (media)
-
-          style.innerHTML = styles
-          elements.set (media, style)
-        })
-    })
-
-    return params
-  }
-}) (new Map ())
+export function update (params = {}) {
+  return insertRule (params)
+}
