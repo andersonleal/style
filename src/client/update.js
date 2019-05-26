c859e6b3e1013d5e0390e718d63bdda90d44cb39
Peter T Bosse II <pbosse@gmail.com>
2019-05-26 09:17:59 -0400

    Remove compact option from client/update

--- src/client/update.js
+++ src/client/update.js
@@ -3,11 +3,11 @@ import { canUseDom, getStyleElement, getStyles, store } from "../api/index.js"
 /* istanbul ignore next */
 
 export const update = (function (elements) {
-  return function (params = {}, compact = true) {
+  return function (params = {}) {
     store.forEach (function (_, media) {
       canUseDom &&
         window.requestAnimationFrame (function () {
-          const styles = getStyles (media, compact)
+          const styles = getStyles (media)
 
           const style = elements.has (media)
             ? elements.get (media)
